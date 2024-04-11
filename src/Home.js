d83ff633b28d942e52e1267a187ada4b7211e485
JunzeTan <tanjunze859@gmail.com>
2024-04-11 10:56:39 -0400

    finish of header

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import  "./Home.css"
+function Home() {
+  return (
+    <div className = "home"> 
+        <div className='home__container'>
+            <img 
+            className='home__image'
+            src="https://files.oaiusercontent.com/file-08pP4VzDlfwBKeSHXHHJfncp?se=2024-04-10T00%3A33%3A21Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D47d54df4-0349-465f-86cd-20157a35c276.webp&sig=qN%2BV6TXAb6yemB9kTxV06ULKGUDj/qf5MJUghQ3Jc0k%3D"
+            alt=''
+            />
+        </div>
+    </div>
+  )
+}
+
+export default Home
