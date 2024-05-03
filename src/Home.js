61012ded8fddb1cd8427abe1c837e2a9ad0335c7
JunzeTan <tanjunze859@gmail.com>
2024-05-03 15:22:27 -0400

    add product class

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,23 @@ function Home() {
   return (
     <div className = "home"> 
         <div className='home__container'>
-            <img 
+            <img
             className='home__image'
-            src="https://files.oaiusercontent.com/file-08pP4VzDlfwBKeSHXHHJfncp?se=2024-04-10T00%3A33%3A21Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D47d54df4-0349-465f-86cd-20157a35c276.webp&sig=qN%2BV6TXAb6yemB9kTxV06ULKGUDj/qf5MJUghQ3Jc0k%3D"
+            src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
             alt=''
             />
+          <div className='home__row'>
+            
+
+          </div>
+          <div className='home__row'>
+
+            
+          </div>
+          <div className='home__row'>
+
+            
+          </div>
         </div>
     </div>
   )
